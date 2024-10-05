b04d884ec5395bd3f3d9435936b8e8a7bb32fa09
Varad J <varad.gcs@gmail.com>
2024-10-06 00:21:08 +0530

    core: Init UserProfile model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserImages(models.Models):
+    file = models.ImageField(upload_to="/user-images")
+
+class Userprofile(models.Model):
+    name = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    images = models.ManyToManyField(UserImages, related_name="user")
+    
