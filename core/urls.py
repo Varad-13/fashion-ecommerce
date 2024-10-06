ea83dd87af9e1033075398e9789a3bc2a7dc80eb
Varad J <varad.gcs@gmail.com>
2024-10-06 18:39:59 +0530

    webstore: Initial models

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index.as_view(), name='index'),
-    path('/login', index.as_view(), name='login'),
-    path('/signup', index.as_view(), name='signup'),
+    path('login/', index.as_view(), name='login'),
+    path('signup/', index.as_view(), name='signup'),
 ]
--- webstore/models.py
+++ webstore/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from core.models import Userprofile
 
-# Create your models here.
+class Category(models.Model):
+    name = models.TextField()
+    thumbnail = models.ImageField(upload_to='category/')
+
+class ProductItem(models.Model):
+    product_image = models.ImageField(upload_to='product/')
+    product_name = models.TextField()
+    price = models.IntegerField()
+
+class VirtualModel(models.Model):
+    model_photo = models.ImageField(upload_to='model/')
+    model_name = models.TextField()
+
+class VirtualPhotos(models.Model):
+    photo = models.ImageField(upload_to='generated-photos/')
+    model = models.ForeignKey(VirtualModel, on_delete=models.CASCADE)
+    product = models.ForeignKey(ProductItem, on_delete=models.CASCADE, related_name='virtual_photos')
+
+class UserPhotos(models.Model):
+    photo = models.ImageField(upload_to='user-photos/')
+    user = models.ForeignKey(Userprofile, on_delete=models.CASCADE, related_name='user_tryon')
+    product = models.ForeignKey(ProductItem, on_delete=models.CASCADE, related_name='user_photos')
