d2465ec3b855e182e3636722b2d4c03776e35541
Varad J <varad.gcs@gmail.com>
2024-10-09 09:59:35 +0530

    Load sales and product newest to oldest

--- webstore/models.py
+++ webstore/models.py
@@ -16,10 +16,14 @@ class ProductItem(models.Model):
     price = models.IntegerField()
     def __str__(self):
         return(self.product_name)
+    class Meta:
+        ordering = ["-id"]
 
 class FlashSale(models.Model):
     product = models.OneToOneField(ProductItem, on_delete=models.CASCADE, related_name='sale')
     price = models.IntegerField()
+    class Meta:
+        ordering = ["-id"]
 
 class VirtualModel(models.Model):
     model_photo = models.ImageField(upload_to='model/')
@@ -31,6 +35,10 @@ class VirtualPhotos(models.Model):
     photo = models.ImageField(upload_to='generated-photos/')
     model = models.ForeignKey(VirtualModel, on_delete=models.CASCADE, related_name='photo')
     product = models.ForeignKey(ProductItem, on_delete=models.CASCADE, related_name='virtual_photos')
+    def __str__(self):
+        model = self.model.model_name
+        product = self.product.product_name
+        return product + model
 
 class UserPhotos(models.Model):
     photo = models.ImageField(upload_to='user-photos/')
